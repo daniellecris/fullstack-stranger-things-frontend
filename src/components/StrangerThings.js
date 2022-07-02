b32a8c1daa18415b1acc1e605fcd906e9b830d18
Danielle Cristina <dnaielle.trybe@gmail.com>
2022-07-02 18:38:36 -0300

    atualizando components

--- src/components/StrangerThings.js
+++ src/components/StrangerThings.js
@@ -2,14 +2,14 @@ import React from 'react';
 import CharactersService from '../services/charactersAPI';
 import Table from './Table';
 
-require('dotenv').config();
+// require('dotenv').config();
 
 const getRealityClass = (hereIsTheUpsideDownWorld) => (
   hereIsTheUpsideDownWorld ? 'upside-down' : 'stranger-things'
 );
 
 const strangerThingsConfig = {
-  url: process.env.REACT_APP_HANKINS_URL,
+  url: process.env.REACT_APP_HAWKINS_URL,
   timeout: +process.env.REACT_APP_HAWKINS_TIMEOUT,
 };
 
